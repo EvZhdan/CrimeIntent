012c1e00d7d55da9d50145e4809b18853e5dca83
EvZhdan <ev107@mail.ru>
2018-03-11 11:39:05 +0300

    first

---
+++ app/src/main/java/biz/httpsoftmobile/crimeintent/CrimeActivity.java
@@ -0,0 +1,13 @@
+package biz.httpsoftmobile.crimeintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
