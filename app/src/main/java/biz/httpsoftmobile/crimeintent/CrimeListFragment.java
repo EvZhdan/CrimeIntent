5b619aa4f0c19310e983495c0f06ab7d51c369df
EvZhdan <ev107@mail.ru>
2018-03-11 19:28:16 +0300

    4

---
+++ app/src/main/java/biz/httpsoftmobile/crimeintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package biz.httpsoftmobile.crimeintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by PC on 3/11/2018.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
---
+++ app/src/main/java/biz/httpsoftmobile/crimeintent/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package biz.httpsoftmobile.crimeintent;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by PC on 3/11/2018.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+}
